6e5aeb8cf60e25339bb370fd3866603b8c7bb0ff
Joaquin <joaquinvaldez101@gmail.com>
2022-08-28 18:25:49 -0600

    Build basic Caesar cipher/decipher functions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # caesar_cipher
+
+A simple implementation of a Caesar Cipher, which takes each character of a message
+and replaces them with a character some fixed number of positions down the
+alphabet. Done in concurrence with the Ruby Basics course on The Odin Project.
---
+++ cipher.rb
@@ -0,0 +1,49 @@
+# 'h'.ord == 104
+# 109.chr == 'm'
+
+def convert_char(char, num)
+    newchar = ''
+    if(char.ord.between?(65,90))
+        if(char.ord + num > 90)
+            newchar = ((char.ord + num)-26).chr
+        elsif(char.ord + num < 65)
+            newchar = ((char.ord + num)+26).chr
+        else
+            newchar = (char.ord + num).chr
+        end
+    elsif(char.ord.between?(97,122))
+        if(char.ord + num > 122)
+            newchar = ((char.ord + num)-26).chr
+        elsif(char.ord + num < 97)
+            newchar = ((char.ord + num)+26).chr
+        else
+            newchar = (char.ord + num).chr
+        end
+    else
+        newchar = char
+    end
+    newchar
+end
+
+
+def caesar_cipher(string, num)
+    encoded_string = ''
+    string.each_char do |char|
+        encoded_string << convert_char(char,num)
+    end
+    [string,encoded_string]
+end
+
+def caesar_decipher(encoded_string, num)
+    decoded_string = ''
+    encoded_string.each_char do |char|
+        decoded_string << convert_char(char, -num)
+    end
+    [encoded_string,decoded_string]
+end
+
+test_string = 'The quick, brown fox jumped over the lazy dog!'
+
+puts caesar_cipher(test_string, 9)
+
+puts caesar_decipher(caesar_cipher(test_string,9)[1], 9)
